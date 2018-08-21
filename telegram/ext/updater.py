59ce6fe7cdf47bacec801f98b517e69e61c4ba5f
vasinkd <vasin.kd@gmail.com>
2018-08-21 20:34:22 +0300

    changes for test

--- telegram/ext/updater.py
+++ telegram/ext/updater.py
@@ -368,7 +368,7 @@ class Updater(object):
         loop = None
         if asyncio:
             loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(asyncio.new_event_loop())
+            asyncio.set_event_loop(loop)
         self.httpd = WebhookServer(port, app, ssl_ctx, loop)
 
         if use_ssl:
